ef358ff45e17b5a824000000a9b3533296814a72
Anna-W123 <103110@student.ue.poznan.pl>
2026-05-24 23:45:54 +0200

    Zadanie 1 - rozwiazanie

--- pesel.py
+++ pesel.py
@@ -29,27 +29,26 @@ def verify_pesel(pesel: str) -> int:
         int: 1 jeśli numer jest poprawny, 0 jeśli nie.
     """
 
-    pesel = input("Podaj PESEL: ")
+pesel = input("Podaj PESEL: ")
 
-    weryfikacja = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3]
+weryfikacja = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3]
 
-    suma = 0
+suma = 0
 
-    for i in range(10):
+for i in range(10):
 
-    	suma = suma + int(pesel[i]) * weryfikacja[i]
+    suma = suma + int(pesel[i]) * weryfikacja[i]
 
-    kontrolna = 10 - (suma % 10)
+kontrolna = 10 - (suma % 10)
 
-    if kontrolna == 10:
+if kontrolna == 10:
     kontrolna = 0
 
-    if kontrolna == int(pesel[10]):
+if kontrolna == int(pesel[10]):
 
-    	print(1)
-    else:
-   	 print(0)
-
+    print(1)
+else:
+   	print(0)
     ### return 0 - powinno być zmienione i zwrócić prawdziwy wynik (zgodny z oczekiwaniami)
     return 0
 
